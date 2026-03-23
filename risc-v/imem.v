111fa4ea425a25f556acd25301a6fbe7da7775b2
dylanlore <dylan.lore@utp.ac.pa>
2026-03-23 09:37:21 -0600

    Correción de funct3 para lw/sw de 3'b011 a 3'b010

--- risc-v/imem.v
+++ risc-v/imem.v
@@ -10,8 +10,8 @@ module imem #(parameter WIDTH = 32)
             // Formato R: {funct7, rs2, rs1, funct3, rd, opcode}
             0: instr = {7'b0000000, 5'd2, 5'd0, 3'b000, 5'd1, 7'b0110011};
             1: instr = {7'b0000000, 5'd2, 5'd0, 3'b000, 5'd3, 7'b0110011};
-            2: instr = {12'd0, 5'd0, 3'b011, 5'd2, 7'b0000011}; 
-            3: instr = {7'd0, 5'd3, 5'd0, 3'b011, 5'd0, 7'b0100011};
+            2: instr = {12'd0, 5'd0, 3'b010, 5'd2, 7'b0000011}; 
+            3: instr = {7'd0, 5'd3, 5'd0, 3'b010, 5'd0, 7'b0100011};
             default: instr = 32'b0;
         endcase
     end
