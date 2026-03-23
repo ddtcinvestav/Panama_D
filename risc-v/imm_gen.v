84c874c5b43ad2958f966fdc04688f266dec03d6
dylanlore <dylan.lore@utp.ac.pa>
2026-03-23 10:05:20 -0600

    Se agregó el opcode 0010011 (addi, ori, andi) al generador de inmediatos.

--- risc-v/imm_gen.v
+++ risc-v/imm_gen.v
@@ -7,16 +7,17 @@ module imm_gen #(parameter WIDTH = 32)
     wire [6:0] opcode = instr[6:0];
     
     always @(*) begin
-        
+
         case(opcode)
             // Bits usados: [31:20]
-            7'b0000011: imm = {{20{instr[31]}}, instr[31:20]};// I-type LOAD
-            // Bits usados: [31:25] (parte alta) y [11:7] (parte baja)
-            7'b0100011: imm = {{20{instr[31]}}, instr[31:25], instr[11:7]};  // S-type STORE
-            // Bits usados: [31] (signo), [7] (bit 11), [30:25] (bits 10:5), [11:8] (bits 4:1)
-            7'b1100011: imm = {{19{instr[31]}}, instr[31], instr[7], instr[30:25], instr[11:8], 1'b0};  // B-type BRANCH
+            7'b0000011: imm = {{20{instr[31]}}, instr[31:20]}; // I-type LOAD (lw)
+            7'b0010011: imm = {{20{instr[31]}}, instr[31:20]}; // I-type ALU (addi, ori, andi)
+            // Bits usados: [31:25] (parte alta) y [11:7] (parte baja)      
+            7'b0100011: imm = {{20{instr[31]}}, instr[31:25], instr[11:7]}; // S-type (sw)
+            // Bits usados: [31] (signo), [7] (bit 11), [30:25] (bits 10:5), [11:8] (bits 4:1)                      
+            7'b1100011: imm = {{19{instr[31]}}, instr[31], instr[7], instr[30:25], instr[11:8], 1'b0};  // B-type (beq)
             default:    imm = 32'b0;
-    endcase
+        endcase
     
     end
     
