eea64551fb94de48ea22004cac87631f6321f722
dylanlore <dylan.lore@utp.ac.pa>
2026-03-27 11:49:05 -0600

    Se añadio WBResult

--- lab3_unicycle_base_mod6/lab3_unicycle_base_mod6.srcs/sources_1/new/cpu.v
+++ lab3_unicycle_base_mod6/lab3_unicycle_base_mod6.srcs/sources_1/new/cpu.v
@@ -2,7 +2,7 @@
 module cpu #(parameter WIDTH = 32, parameter RESET_ADDR = 32'h00000000)(
 
 		input clk,
-		input arstn, //Cambio el RST a Activo bajo
+		input arstn, //Cambio el RST a Activo bajo.
 		
 		//ROM externa
 		output wire [WIDTH-1:0] inst_mem_r_addr,
