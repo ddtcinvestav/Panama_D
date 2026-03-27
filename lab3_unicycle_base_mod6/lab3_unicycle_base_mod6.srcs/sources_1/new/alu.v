a6cd759429c8880e35fd76b80bf731257f389bf0
dylanlore <dylan.lore@utp.ac.pa>
2026-03-27 12:09:54 -0600

    Se añadieron operaciones nuevas (XOR,SLL,SRL,SRA,SLT,SLTU)

--- lab3_unicycle_base_mod6/lab3_unicycle_base_mod6.srcs/sources_1/new/alu.v
+++ lab3_unicycle_base_mod6/lab3_unicycle_base_mod6.srcs/sources_1/new/alu.v
@@ -8,10 +8,17 @@ module alu #(parameter WIDTH = 32)
 );
     always @(*) begin
         case(seleccion)
-            4'b0000: resultado = A & B;  // AND
-            4'b0001: resultado = A | B;  // OR  
-            4'b0010: resultado = A + B;  // ADD
-            4'b0110: resultado = A - B;  // SUB
+            4'b0000: resultado = A & B;                             // AND
+            4'b0001: resultado = A | B;                             // OR  
+            4'b0010: resultado = A + B;                             // ADD
+            4'b0110: resultado = A - B;                             // SUB
+            //Operaciones nuevas añadidas
+            4'b0011: resultado = A ^ B;                             // XOR
+            4'b0100: resultado = A << B[4:0];                       // SLL
+            4'b0101: resultado = A >> B[4:0];                       // SRL
+            4'b0111: resultado = $signed(A) >>> B[4:0];             // SRA
+            4'b1000: resultado = ($signed(A) < $signed(B)) ? 1 : 0; // SLT
+            4'b1001: resultado = (A < B) ? 1 : 0;                   // SLTU
             default: resultado = 32'b0;
         endcase
     end
