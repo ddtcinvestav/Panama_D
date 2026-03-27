982e1203cb1edd18af5005348c77000d03c4a6ec
dylanlore <dylan.lore@utp.ac.pa>
2026-03-27 11:50:06 -0600

    Se cambio a RST Activo Bajo

--- lab3_unicycle_base_mod6/lab3_unicycle_base_mod6.srcs/sources_1/new/regfile.v
+++ lab3_unicycle_base_mod6/lab3_unicycle_base_mod6.srcs/sources_1/new/regfile.v
@@ -1,7 +1,7 @@
 module regfile #(parameter WIDTH = 32, parameter DEPTH = 32, parameter ADDR = 5) 
 (
     input clk,
-    input arstn, // Cambio el RST a Activo bajo
+    input arstn, // Cambio el RST a Activo bajo.
     input we,             // Write Enable
     input [ADDR-1:0] rs1, // Indice registro 1
     input [ADDR-1:0] rs2, // Indice registro 2
