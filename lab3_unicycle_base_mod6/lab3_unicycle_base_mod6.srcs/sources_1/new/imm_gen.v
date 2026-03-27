b22aa74e01801a2d79ca2b8333e4f33c172d103e
dylanlore <dylan.lore@utp.ac.pa>
2026-03-27 12:18:45 -0600

    Se añadieron sus equivalentes de control (OP_IMM, LUI, AUIPC, JAL, JALR)

--- lab3_unicycle_base_mod6/lab3_unicycle_base_mod6.srcs/sources_1/new/imm_gen.v
+++ lab3_unicycle_base_mod6/lab3_unicycle_base_mod6.srcs/sources_1/new/imm_gen.v
@@ -15,6 +15,22 @@ module imm_gen #(parameter WIDTH = 32)
             7'b0100011: imm = {{20{instr[31]}}, instr[31:25], instr[11:7]};  // S-type STORE
             // Bits usados: [31] (signo), [7] (bit 11), [30:25] (bits 10:5), [11:8] (bits 4:1)
             7'b1100011: imm = {{19{instr[31]}}, instr[31], instr[7], instr[30:25], instr[11:8], 1'b0};  // B-type BRANCH
+
+            //Añadidas nuevas
+            // I-type OP-IMM (addi, ori, etc.)
+            7'b0010011: imm = {{20{instr[31]}}, instr[31:20]};
+
+            // J-type JAL
+            7'b1101111: imm = {{12{instr[31]}}, instr[19:12], instr[20], instr[30:21], 1'b0};
+
+            // I-type JALR
+            7'b1100111: imm = {{20{instr[31]}}, instr[31:20]};
+
+            // U-type LUI y AUIPC
+            7'b0110111: imm = {instr[31:12], 12'h000};  // LUI
+            7'b0010111: imm = {instr[31:12], 12'h000};  // AUIPC
+
+
             default:    imm = 32'b0;
     endcase
 
