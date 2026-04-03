9f5dd13022f5e2e5df2572ec34452401d25c042f
dylanlore <dylan.lore@utp.ac.pa>
2026-04-03 11:25:50 -0600

    Se creo el modulo top, se instanciaron los modulos cpu, imem, dmem y se crearon los wires para conectar imem y dmem

--- lab3_unicycle_base_mod6/lab3_unicycle_base_mod6.srcs/sources_1/new/top.v
+++ lab3_unicycle_base_mod6/lab3_unicycle_base_mod6.srcs/sources_1/new/top.v
@@ -1,26 +1,50 @@
-`timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 03.04.2026 11:06:03
-// Design Name: 
-// Module Name: top
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
+module top #(parameter WIDTH = 32, parameter RESET_ADDR = 32'h00000000)(
+      input clk,
+      input arstn,
+      output valid
+  );
 
+      // Wires para conectar cpu con imem
+      wire [WIDTH-1:0] inst_addr;
+      wire [WIDTH-1:0] inst_data;
 
-module top(
+      // Wires para conectar cpu con dmem
+      wire             mem_write;
+      wire             mem_read;
+      wire [WIDTH-1:0] data_addr;
+      wire [WIDTH-1:0] data_write;
+      wire [WIDTH-1:0] data_read;
+      wire [1:0]       write_strb;
 
-    );
-endmodule
+      // Instancia CPU
+      cpu #(.WIDTH(WIDTH), .RESET_ADDR(RESET_ADDR)) CPU (
+          .clk                    (clk),
+          .arstn                  (arstn),
+          .inst_mem_r_addr        (inst_addr),
+          .inst_memory_in         (inst_data),
+          .data_memory_write      (mem_write),
+          .data_memory_read       (mem_read),
+          .data_address_out       (data_addr),
+          .data_memory_write_data (data_write),
+          .data_memory_in         (data_read),
+          .data_memory_write_strb (write_strb),
+          .valid                  (valid)
+      );
+
+      // Instancia IMEM
+      imem #(.WIDTH(WIDTH)) IMEM (
+          .addr  (inst_addr),
+          .instr (inst_data)
+      );
+
+      // Instancia DMEM
+      dmem #(.WIDTH(WIDTH)) DMEM (
+          .clk  (clk),
+          .we   (mem_write),
+          .re   (mem_read),
+          .addr (data_addr),
+          .wd   (data_write),
+          .rd   (data_read)
+      );
+
+  endmodule
